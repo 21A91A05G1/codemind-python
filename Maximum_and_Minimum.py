a2cad936037ad33f70561df8fd94bc18f63a5016
21A91A05G1 <97424960+21A91A05G1@users.noreply.github.com>
2022-06-28 20:36:41 +0530

    n=int(input()) arr=list(map(int,input().split())) b=[] d=[] c=0 for i in arr:     if i==arr.count(i):         c+=1         b.append(i) if(c==0):     print("-1") else:     print(min(b),max(b),end=" ")

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+b=[]
+d=[]
+c=0
+for i in arr:
+    if i==arr.count(i):
+        c+=1
+        b.append(i)
+if(c==0):
+    print("-1")
+else:
+    print(min(b),max(b),end=" ")
